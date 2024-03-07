ba5e5264812dcdb1c703c7ad763bd2b5cd7ff382
Hashim-ca <hashim.ik217@gmail.com>
2024-03-07 14:02:41 -0700

    updates

--- app/src/androidTest/java/com/example/lab8/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lab8/ExampleInstrumentedTest.java
@@ -1,7 +1,16 @@
 package com.example.lab8;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -16,6 +25,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
@@ -23,4 +33,12 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.lab8", appContext.getPackageName());
     }
+
+    @Test
+    public void hasCity(){
+        //check if the city name is Edmonton
+        onView(withId(R.id.city_text)).check(matches(withText("Edmonton")))
+    }
+
+
 }
